ad8169cce63549afa0cc9b385dc06ec7cd2a9a6a
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-07-06 17:13:37 -0500

    added ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps : [{
+    name: 'FEC',
+    script: './server/server.js',
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
