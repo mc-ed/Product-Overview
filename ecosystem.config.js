50379e94015829ee0cc9daae8e430105a9327c47
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-07-09 11:10:51 -0500

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+      name: 'Product-Overview',
+      script: './index.js'
+    }],
+    deploy: {
+      production: {
+        user: 'ubuntu',
+        host: 'ec2-18-188-213-241.us-east-2.compute.amazonaws.com',
+        key: '~/.ssh/FECPO.pem',
+        ref: 'origin/master',
+        repo: 'https://github.com/mc-ed/Product-Overview.git',
+        path: '/home/ubuntu/server',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
