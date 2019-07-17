620f8374e3a98a9117d23c83d19d2a4d3e92ed4f
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-07-16 20:25:19 -0500

    Add Star Rendering function

--- client/components/Ratings.jsx
+++ client/components/Ratings.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
 const Ratings = (props) => {
+    const star = '\uECE2';
+    const half = '\uECE1';
+
+    const starString;
+    const avgRating = props.avgRating;
+
+    while (avgRating > 0) {
+        if (Math.floor(avgRating) === avgRating) {
+            starString += star;
+            avgRating --;
+        } else {
+            starString += half;
+            avgRating = avgRating - .5;
+        }    
+    }
+
+    
     return (
         <div class="col Ratings">
             <p class="ratingsLink">{props.ratings} Ratings</p>
-            <p class="icons stars">{'\uECE2'}{'\uECE2'}{'\uECE2'}{'\uECE2'}{'\uECE1'}</p>
+            <p class="icons stars">{starString}</p>
             <p class="averageRating">{props.avgRating} Average</p>
 
         </div>
