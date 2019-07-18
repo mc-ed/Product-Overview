d611839d1c76f6a5f0fbacd180dfe526df1674e5
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-07-18 17:00:05 -0500

    added zoom in modals

--- client/components/Box.jsx
+++ client/components/Box.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import ReactImageZoom from 'react-image-zoom'
 
 const Box = (props) => {
    
@@ -18,13 +18,14 @@ const Box = (props) => {
    
     const [mainImage, setMainImage] = useState(props.src);
     const [imageTag, setImageTag] = useState(null);
-
+    const zoomProps = {width: 300, zoomWidth: 250, img: mainImage}
    
     return (
         <div className="container">
             <div className="row">
                 <div className="tnList" onClick={changePhoto} className="col-2">{images}</div>
-                <img ref={input => setImageTag(input)} className="col-9 main-modal-image" style={{width: 200 + "px", height: 300 +"px"}} src={mainImage}></img>
+                {/* <img ref={input => setImageTag(input)} className="col-9 main-modal-image" style={{width: 200 + "px", height: 300 +"px"}} src={mainImage}></img> */}
+                <ReactImageZoom ref={input => setImageTag(input)} className="col-10 main-modal-image" src={mainImage}{...zoomProps} />
                 {/* <div className="col-1">
 
                     <button onClick={zoomIn}>{'\u005A'}</button>
