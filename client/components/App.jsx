421b3465a09463a61af62c49ac125f35ebc08209
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-07-17 20:27:59 -0500

    send price as number to cart

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -67,7 +67,7 @@ class App extends Component {
             name: this.state.name,
             id: this.state.SS,
             amount: this.state.quantity,
-            price: this.state.price
+            price: Number(this.state.price)
         }}))
     
     }
