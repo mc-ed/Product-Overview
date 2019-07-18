fd63b69448ca3f86cdf5b22aec722f36eb78bfd5
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-07-18 15:24:06 -0500

    more responsiveness, format thumbnails, disable quantity decrease if quantity is less than 2

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 
 const Cart = (props) => {
+
+    if (props.quantity < 2) {
+        return (
+            <div className="row no-gutters Cart">
+                <span className="quantity col-sm-6">
+                    <button className="col-3 minusOne-gray">{'\ued5b'}</button>
+                    <input type="text" className="col-3" onChange={props.typeQuantity} value={props.quantity}/>
+                    <button className="col-3 plusOne" onClick={props.plusOne}>{'\ued5a'}</button>
+                </span>
+                <span className="cartButton col-sm-6">
+                    <button className="col-12 addToCart" onClick={props.cartListener}>ADD TO CART</button>
+                </span>
+       
+            </div>
+
+        )
+    }
     
     return (
-    <div className="row no-gutters Cart">
-        <span className="quantity col-sm-6">
-            <button className="col-3 minusOne" onClick={props.minusOne}>{'\ued5b'}</button>
-            <input type="text" className="col-3" onChange={props.typeQuantity} value={props.quantity}/>
-            <button className="col-3 plusOne" onClick={props.plusOne}>{'\ued5a'}</button>
-        </span>
-        <span className="cartButton col-sm-6">
-            <button className="col-12 addToCart" onClick={props.cartListener}>ADD TO CART</button>
-        </span>
-       
-    </div>)
+        <div className="row no-gutters Cart">
+            <span className="quantity col-sm-6">
+                <button className="col-3 minusOne" onClick={props.minusOne}>{'\ued5b'}</button>
+                <input type="text" className="col-3" onChange={props.typeQuantity} value={props.quantity}/>
+                <button className="col-3 plusOne" onClick={props.plusOne}>{'\ued5a'}</button>
+            </span>
+            <span className="cartButton col-sm-6">
+                <button className="col-12 addToCart" onClick={props.cartListener}>ADD TO CART</button>
+            </span>
+        
+        </div>
+    )
 
 }
 
