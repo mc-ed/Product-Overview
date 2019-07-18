29ac29e43913a2db5f4548638c2083c52edea23f
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-07-18 12:07:50 -0500

    resized cart elements in rightBox

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -4,13 +4,17 @@ const Cart = (props) => {
     
     return (
     <div className="row no-gutters Cart">
-        <button className="col-4 col-md-2 minusOne" onClick={props.minusOne}>{'\ued5b'}</button>
-        <input type="text" className="col-4 col-md-2" onChange={props.typeQuantity} value={props.quantity}/>
-        <button className="col-4 col-md-2 plusOne" onClick={props.plusOne}>{'\ued5a'}</button>
-        <button className="col-12 col-md-6 addToCart" onClick={props.cartListener}>ADD TO CART</button>
+        <span className="quantity col-6">
+            <button className="col-3 minusOne" onClick={props.minusOne}>{'\ued5b'}</button>
+            <input type="text" className="col-3" onChange={props.typeQuantity} value={props.quantity}/>
+            <button className="col-3 plusOne" onClick={props.plusOne}>{'\ued5a'}</button>
+        </span>
+        <span className="cartButton col-6">
+            <button className="col-12 addToCart" onClick={props.cartListener}>ADD TO CART</button>
+        </span>
        
     </div>)
 
 }
 
-export default Cart;
+export default Cart; 
