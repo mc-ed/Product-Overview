8117a94464bdea296892fdf36cf25f5f7cfa266b
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-07-18 10:50:18 -0500

    the stars at night are rendering right...

--- client/components/Ratings.jsx
+++ client/components/Ratings.jsx
@@ -1,29 +1,45 @@
 import React from 'react';
 
 const Ratings = (props) => {
-    const star = '\uECE2';
-    const half = '\uECE1';
-
-    let starString = '';
-    let avgRating = props.avgRating;
+    //set array of empty stars
+    const getStarString = (num) => {
     
-    while (avgRating >= .5) {
-       
-        if (avgRating >= 1) {
+        let counter = num;
+        const starString = ['\uECE0', '\uECE0', '\uECE0', '\uECE0', '\uECE0'];
+        
+        
+        
+        let stringPointer = 0
+        //loop through size of avg rating
+        while (counter > 0 && stringPointer <= 5) {
             
-            starString += star;
-            avgRating--;
-        } else {
-            starString += half;
-            avgRating = avgRating - .5;
-        }    
+            //if the remaining rating is a larger than 0 but less than 1
+            if (counter < 1) {
+                //replace current empty stars with a half star
+                starString[stringPointer] = '\uECE1';
+                counter = 0
+                //return empty stars.join
+            }
+            //if the remaining rating is greater or equal to 1
+            if (counter >= 1) {
+                //replace current empty stars with a full star
+                starString[stringPointer] = '\uECE2';
+                //decrement avg rating
+                counter--;
+                stringPointer++;
+            }
+        }
+        console.log(starString)
+        return starString.join('');
     }
+    
+    
     
     return (
         <div className="col Ratings">
         
             <p className="ratingsLink">{props.ratings} Ratings</p>
-            <p className="icons">{starString}</p>
+            <p className="icons">{getStarString(props.avgRating)}</p>
             <p className="averageRating">{props.avgRating} Average</p>
 
         </div>
