d6d1720077a151c8fd3d2e4a4763e5fdc0aa0307
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-08-19 12:32:07 -0500

    update deployed routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -15,7 +15,7 @@ class App extends Component {
             ratings: 0,
             avgRating: 0,
             percentRecommended: 0,
-            
+
             images: [],
             price: 0,
             summary: [],
@@ -24,8 +24,8 @@ class App extends Component {
             total: 0,
             clickedItems: new Array(100).fill(0),
             loggedIn: false
-            } 
-        
+            }
+
         this.plusOne = this.plusOne.bind(this);
         this.minusOne = this.minusOne.bind(this);
         this.typeQuantity = this.typeQuantity.bind(this);
@@ -34,7 +34,7 @@ class App extends Component {
         this.getRatings = this.getRatings.bind(this);
         this.cartListener = this.cartListener.bind(this);
         this.sendSaveInfo = this.sendSaveInfo.bind(this);
-      
+
     };
 
     plusOne(event) {
@@ -72,16 +72,16 @@ class App extends Component {
             amount: this.state.quantity,
             price: Number(this.state.price.replace(/[^0-9.-]+/g, ''))
         }}))
-    
+
     };
 
     sendSaveInfo () {
-        
+
         if (this.state.clickedItems[this.state.SS]) {
             let clickedItems = this.state.clickedItems;
             clickedItems[this.state.SS] = 0;
             this.setState({clickedItems})
-           
+
         } else {
             let clickedItems = this.state.clickedItems;
             clickedItems[this.state.SS] = this.state.SS;
@@ -91,12 +91,12 @@ class App extends Component {
         window.dispatchEvent(
             new CustomEvent('favorite', {detail: {product_id: this.state.SS, price: this.state.price, name: this.state.name, saved: this.state.clickedItems[this.state.SS]}})
         )
-       
+
     }
 
-    
+
     componentDidMount () {
-        
+
         window.addEventListener('product', e => {
             const SS = e.detail.product_id;
             this.setState({ SS }, () => {
@@ -104,19 +104,19 @@ class App extends Component {
                 this.getItems()
                 })
        })
-        
+
         window.addEventListener('stars', (e) => {
             const SS = e.detail.id
-                
+
             this.setState({SS}, () => {
                 this.getRatings();
                 this.getItems();
-            })     
+            })
         })
        window.addEventListener('loggedIn', (e) => {
         const clickedItems = e.detail.favoriteList;
         const loggedIn = e.detail.loggedIn
-        
+
         //if the id of the item is in the userFavorites
         if (clickedItems.includes(this.state.SS)) {
             this.setState({clickedItems, loggedIn})
@@ -125,10 +125,10 @@ class App extends Component {
             //keep the button as if it were clicked once
         if (!this.state.clickedItems[this.state.SS]) {
             this.setState({clickedItems, loggedIn})
-            
+
         }
         });
-    
+
         window.addEventListener('loggedOut', (e) => {
             console.log(e.detail)
             const clickedItems = new Array(100).fill(0);
@@ -139,9 +139,9 @@ class App extends Component {
    };
 
    getRatings () {
-      
-       axios.get(`http://ec2-18-225-6-113.us-east-2.compute.amazonaws.com/api/stats/${this.state.SS}`)
-        .then(results => 
+
+       axios.get(`http://ec2-18-223-44-5.us-east-2.compute.amazonaws.com/api/stats/${this.state.SS}`)
+        .then(results =>
 
             this.setState({
                 ratings: results.data.reviewStats.reviewCount,
@@ -154,15 +154,15 @@ class App extends Component {
 
    getItems () {
        const SS = this.state.SS;
-        
-       axios.get(`http://ec2-18-188-213-241.us-east-2.compute.amazonaws.com/${SS}`)
+
+       axios.get(`ec2-18-223-44-5.us-east-2.compute.amazonaws.com/${SS}`)
        .then(results => {
             const editedURLs = results.data.largeImages.filter(url => url.slice(-3) === 'jpg')
             const items = editedURLs.map((file) =>{
-               
+
                return `https://binkardfecimages.s3.us-east-2.amazonaws.com/FECPhotos/${results.data.SS}/${file}`
             })
-           
+
             this.setState({
                 SS: results.data.SS,
                 itemNumber: results.data.itemNumber,
@@ -177,25 +177,25 @@ class App extends Component {
                 quantity: 1,
                 items: [],
                 total: 0,
-               
+
             })
         }).catch(err => console.log(err))
    };
-   
+
     render() {
-       
+
         return (
-            
+
             <div className="row no-gutters">
-                
+
                 <LeftBox itemNumber={this.state.itemNumber} modelNumber ={this.state.modelNumber} name={this.state.name} ratings={this.state.ratings}
                 avgRating={this.state.avgRating} percentRecommended={this.state.percentRecommended}
                 images={this.state.images}/>
                 <RightBox loggedIn={this.state.loggedIn} price={this.state.price} sendSaveInfo={this.sendSaveInfo} clickedItems={this.state.clickedItems} name={this.state.name} id={this.state.SS} summary={this.state.summary} quantity={this.state.quantity}
-                plusOne={this.plusOne} minusOne={this.minusOne} typeQuantity={this.typeQuantity} 
+                plusOne={this.plusOne} minusOne={this.minusOne} typeQuantity={this.typeQuantity}
                 addToCart={this.addToCart} cartListener={this.cartListener}/>
             </div>
-            
+
         )
     };
 };
